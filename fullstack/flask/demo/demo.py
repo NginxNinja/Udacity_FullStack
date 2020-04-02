837ec60a81f2cdd2239bbf5222692bfab671fef9
Nginx Ninja <dummy.kenmen2@gmail.com>
2020-04-02 10:35:46 +0800

    Ended on Lesson 4: Part 1 - SQL and Data Modeling for the Web
     Changes to be committed:
    	modified:   fullstack/Vagrantfile
    	new file:   fullstack/bookmark.html
    	new file:   fullstack/cheatsheet/postgres-psql-cheat-sheet.pdf
    	modified:   fullstack/flask/demo/demo.py

--- fullstack/flask/demo/demo.py
+++ fullstack/flask/demo/demo.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vagrant:Welcome1@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello, World! World. World! Mwahaha! What the Heck!'
+    person = Person.query.first()
+    return f'Hello, {person.name}!'
 
 if __name__ == '__main__':
     # host param - Set this to '0.0.0.0' to have the server available externally as well. For the Vagrant VMbox forwarded_port to be accessible to the Host browser via localhost.
