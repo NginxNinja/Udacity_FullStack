4d7baeb7c8f32cf546954bbf76396a13afaf6e42
Nginx Ninja <dummy.kenmen2@gmail.com>
2020-04-04 11:04:47 +0800

    Ends on Lesson 6: Part 1 - SQL and Data Modeling for the Web
     Changes to be committed:
    	modified:   fullstack/bookmark.html
    	new file:   fullstack/todoapp/app.py
    	new file:   fullstack/todoapp/templates/index.html

---
+++ fullstack/todoapp/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Welcome1@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todo/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+''' # Original codes without handling sessions in controllers using try/except/finally blocks
+def create_todo():
+    #description = request.form.get('description', '') // the original request.form object without calling JSON
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+
+    #return redirect(url_for('index')) // the original code without using JSON
+    return jsonify({
+        'description': todo.description
+    })
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
