22063cf0dbd661215ad150a9a5c5df25cc172d61
Nginx Ninja <dummy.kenmen2@gmail.com>
2020-04-05 11:04:20 +0800

    Ends on Lesson 7 (Build a CRUD App with SQLAlchemy ORM - Part 2): Part 3 - SQL and Data Modeling for the Web
     Changes to be committed:
    	modified:   fullstack/Vagrantfile
    	modified:   fullstack/bookmark.html
    	modified:   fullstack/todoapp/app.py
    	new file:   fullstack/todoapp/migrations/README
    	new file:   fullstack/todoapp/migrations/alembic.ini
    	new file:   fullstack/todoapp/migrations/env.py
    	new file:   fullstack/todoapp/migrations/script.py.mako
    	new file:   fullstack/todoapp/migrations/versions/06ed814d7b97_.py
    	new file:   fullstack/todoapp/migrations/versions/0e13d694597d_.py
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/ReadMe.txt
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations.zip
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/app.py
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/README
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/alembic.ini
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/env.py
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/script.py.mako
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/versions/1f4910056ae8_.py
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/migrations/versions/2f89f5b65743_.py
    	new file:   fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/todoapp-migrations/templates/index.html
    	modified:   fullstack/todoapp/templates/index.html

--- fullstack/todoapp/app.py
+++ fullstack/todoapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -8,15 +9,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# db.create_all() //This method can only be used if not using the Flask-Migrate library.
 
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
@@ -39,7 +43,7 @@ def create_todo():
     else:
         return jsonify(body)
 
-''' # Original codes without handling sessions in controllers using try/except/finally blocks
+''' # Original version without handling sessions in controllers using try/except/finally blocks
 def create_todo():
     #description = request.form.get('description', '') // the original request.form object without calling JSON
     description = request.get_json()['description']
@@ -53,9 +57,26 @@ def create_todo():
     })
 '''
 
+@app.route('/todo/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 if __name__ == '__main__':
+    # host param - Set this to '0.0.0.0' to have the server available externally as well. For the Vagrant VMbox forwarded_port to be accessible to the Host browser via localhost.
+    # port param - the port of the webserver. Defaults to 5000 or the port defined in the SERVER_NAME config variable if present.
+    # debug param - As such to enable just the interactive debugger without the code reloading, you have to invoke run() with debug=True and use_reloader=False.
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ fullstack/todoapp/other_file/todoapp-migrations(Lesson 6-Migrations)/ReadMe.txt
@@ -0,0 +1,2 @@
+Files from the instructor after the SQL and Data Modeling for the Web: Lesson 6 - Migrations
+Finished on 04/05/2020 at 07:50
