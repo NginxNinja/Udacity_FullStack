837836446b56fedc04b325e3f179ead822c9ccb4
Nginx Ninja <dummy.kenmen2@gmail.com>
2020-04-19 15:19:14 +0800

    Ends at Lesson 2 (Identity and Authentication) Part 16 - Identity and Access Management
    	modified:   BasicFlaskAuth/app.py
    	modified:   Vagrantfile
    	new file:   authentication/jwt/auth0_sample02.py
    	modified:   authentication/jwt/miscellaneous.txt
    	new file:   venv/Lib/site-packages/crypto/
    	deleted:    venv_auth0/

---
+++ fullstack/authentication/jwt/auth0_sample02.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, abort
+from functools import wraps
+
+app = Flask(__name__)
+
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    return header_parts[1]
+
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header()
+        return f(jwt, *args, **kwargs)
+    return wrapper
+
+@app.route('/headers')
+@requires_auth
+def headers(jwt):
+    # @TODO unpack the request header
+    
+    print(jwt)
+    return 'not implemented'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
